ce8cf7e0ccf6a3ff3563cdd060d4b8592e4dd8dd
Al Avery <averyab0@sewanee.edu>
2017-08-05 21:00:24 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+SEEN_EMAIL_FILE_NAME = "seen_email_data.json"
+ADDRESSES_JSON_FILE_NAME = "unused_addresses.json"
+LOG_FILE_PATH = "logging.txt"
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/gmail.send','https://www.googleapis.com/auth/gmail.modify']
+CLIENT_SECRET_FILE_NAME = 'client_secret.json'
+APPLICATION_NAME = 'TellOnKlein'
+APPLICATION_EMAIL = 'tellonklein@gmail.com'
+
+NEW_ADDRESSES_SUBJECT = "Your Mailing Addresses"
+REJECTION_SUBJECT = "Sorry"
+NEW_ADDRESSES_TEXT = "Here are 10 addresses:"
+REJECTION_TEXT = "You have not sent an image of a postcard since receiving ten addresses previously."
+
+CAPITULATION_TEXT = "code red"
