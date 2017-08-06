dbae52790fcfd024720e4785b2d3d2397f697f37
Al Avery <averyab0@sewanee.edu>
2017-08-05 20:37:20 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,10 +1,28 @@
 from core_logic import *
 from file_io import *
 from config import *
+import random
+
+
+# unused_addresses = get_json_data("Batch1 - Two Votes.json")
+# seen_email_data = []
 
 
 def make_an_email(address, attach, txt):
 	 return {'email_address':address,'has_attachment':attach,'text_content':txt}
-an_email = make_an_email("abc@gmail",False,"Hey")
-unused_addresses = get_json_data("Batch1 - Two Votes.json")
-seen_email_data = []
+
+
+def generate_email_data():
+	address_chooser = random.randint(0,10)
+	if address_chooser < 8:
+		email_address = str(random.randint(0,3)) + "@gmail.com"
+	else:
+		email_address = str(random.randint(4,7)) + "@gmail.com"
+	
+	attachment = random.choice([True, False])
+	txt_chooser = random.randint(0,10)
+	if txt_chooser < 7:
+		txt = "hello"
+	else:
+		txt = "code red"
+	return make_an_email(email_address, attachment, txt)
