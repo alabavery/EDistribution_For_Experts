7782f66a2e60a66b91311c84680b98ae0cc2f6ed
Al Avery <averyab0@sewanee.edu>
2017-08-09 09:15:07 -0500

    Add reset_json_files()

--- file_io.py
+++ file_io.py
@@ -23,3 +23,9 @@ def write_json_data(json_file_path, data):
 	json_file = open(json_file_path, 'w')
 	json_file.write(data_json)
 	json_file.close()
+
+
+def reset_json_files(csv_file_path, unused_addresses_file_path, seen_email_data_file_path):
+	unused_addresses = get_one_column_csv(csv_file_path)
+	write_json_data(unused_addresses_file_path, unused_addresses)
+	write_json_data(seen_email_data_file_path, [])
